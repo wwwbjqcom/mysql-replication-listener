13c04980ae74a59932a7a1dd5271881b5050452b
Mak Inada <github@inada.com>
2015-04-24 02:01:04 +0000

    @B659:R unescape user, pass and host given as an URI.

--- src/access_method_factory.cpp
+++ src/access_method_factory.cpp
@@ -25,6 +25,135 @@ using mysql::system::Binary_log_driver;
 using mysql::system::Binlog_tcp_driver;
 using mysql::system::Binlog_file_driver;
 
+/*
+ * The following code snippet is from:
+ * http://www.codeguru.com/cpp/cpp/algorithms/strings/article.php/c12759/URI-Encoding-and-Decoding.htm
+ */
+const char HEX2DEC[256] =
+
+{
+
+    /*       0  1  2  3   4  5  6  7   8  9  A  B   C  D  E  F */
+
+    /* 0 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 1 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 2 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 3 */  0, 1, 2, 3,  4, 5, 6, 7,  8, 9,-1,-1, -1,-1,-1,-1,
+
+
+
+    /* 4 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 5 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 6 */ -1,10,11,12, 13,14,15,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 7 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+
+
+    /* 8 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* 9 */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* A */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* B */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+
+
+    /* C */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* D */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* E */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1,
+
+    /* F */ -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1, -1,-1,-1,-1
+
+};
+
+std::string UriDecode(const std::string & sSrc)
+
+{
+
+    // Note from RFC1630:  "Sequences which start with a percent sign
+
+    // but are not followed by two hexadecimal characters (0-9, A-F) are reserved
+
+    // for future extension"
+
+
+
+    const unsigned char * pSrc = (const unsigned char *)sSrc.c_str();
+
+	const int SRC_LEN = sSrc.length();
+
+    const unsigned char * const SRC_END = pSrc + SRC_LEN;
+
+    const unsigned char * const SRC_LAST_DEC = SRC_END - 2;   // last decodable '%'
+
+
+
+    char * const pStart = new char[SRC_LEN];
+
+    char * pEnd = pStart;
+
+
+
+    while (pSrc < SRC_LAST_DEC)
+
+	{
+
+		if (*pSrc == '%')
+
+        {
+
+            char dec1, dec2;
+
+            if (-1 != (dec1 = HEX2DEC[*(pSrc + 1)])
+
+                && -1 != (dec2 = HEX2DEC[*(pSrc + 2)]))
+
+            {
+
+                *pEnd++ = (dec1 << 4) + dec2;
+
+                pSrc += 3;
+
+                continue;
+
+            }
+
+        }
+
+
+
+        *pEnd++ = *pSrc++;
+
+	}
+
+
+
+    // the last 2- chars
+
+    while (pSrc < SRC_END)
+
+        *pEnd++ = *pSrc++;
+
+
+
+    std::string sResult(pStart, pEnd);
+
+    delete [] pStart;
+
+	return sResult;
+
+}
+
+
 /**
    Parse the body of a MySQL URI.
 
@@ -71,13 +200,13 @@ static Binary_log_driver *parse_mysql_url(const char *body, size_t len)
   unsigned long portno = 3306;
   if (*host_end == ':')
     portno = strtoul(host_end + 1, NULL, 10);
+  std::string user_str = UriDecode(std::string(user, user_end - user));
+  std::string pass_str = UriDecode(std::string(pass, pass_end - pass));
+  std::string host_str = UriDecode(std::string(host, host_end - host));
 
   /* Host name is now the string [host, port-1) if port != NULL and [host, EOS) otherwise. */
   /* Port number is stored in portno, either the default, or a parsed one */
-  return new Binlog_tcp_driver(std::string(user, user_end - user),
-                               std::string(pass, pass_end - pass),
-                               std::string(host, host_end - host),
-                               portno);
+  return new Binlog_tcp_driver(user_str, pass_str, host_str, portno);
 }
 
 
